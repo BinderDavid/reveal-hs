aa8f537c32d889e18b9edde0948ef0d99167d9a7
BinderDavid <dbinder1989@googlemail.com>
2020-08-28 22:11:18 +0200

    Add transitions

--- src/Library.hs
+++ src/Library.hs
@@ -10,6 +10,7 @@ module Library
 import Text.Blaze.Html
 import Text.Blaze.Html5
 import Text.Blaze.Html5.Attributes hiding (title)
+import Text.Blaze.Internal (attribute)
 
 import Control.Monad (forM_)
 import Data.String (fromString)
@@ -91,6 +92,56 @@ slidesBody slides = do
     ]
 
 
+
+
+foo :: Html -> Slide
+foo h = section ! dataTransition (STransition Zoom) ! dataTransitionSpeed Slow $ h
+
 slidesToHtml :: [Slide] -> Html
-slidesToHtml slides = forM_  slides section
+slidesToHtml slides = forM_  slides foo
+
+
+------------------------------------------------
+-- Transitions
+
+dataTransition :: Transition -> Attribute
+dataTransition tr = attribute "data-transition" " data-transition=\"" (fromString . show $ tr)
+
+dataTransitionSpeed :: TransitionSpeed -> Attribute
+dataTransitionSpeed trs = attribute "data-transition-speed" " data-transition-speed=\"" (fromString . show $ trs)
+
+data TransitionSpeed
+  = Default
+  | Fast
+  | Slow
+
+instance Show TransitionSpeed where
+  show Default = "default"
+  show Fast    = "fast"
+  show Slow    = "slow"
+
+data SimpleTransition
+  = None
+  | Fade
+  | Slide
+  | Convex
+  | Concave
+  | Zoom
+
+instance Show SimpleTransition where
+  show None    = "none"
+  show Fade    = "fade"
+  show Slide   = "slide"
+  show Convex  = "convex"
+  show Concave = "concave"
+  show Zoom    = "zoom"
+
+data Transition
+  = STransition SimpleTransition
+  | InOutTransition SimpleTransition SimpleTransition
+
+instance Show Transition where
+  show (STransition st) = show st
+  show (InOutTransition st st') = show st ++ "-in " ++ show st' ++ "-out"
+
 
